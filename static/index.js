0ef411a4871100b45710c515cf2501930bf45aba
Leodeng <71826908+leodenglovescode@users.noreply.github.com>
2022-09-23 13:42:25 +0800

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -124,7 +124,7 @@ function gameRestart() {
     _gameScore = 0;
     _gameOver = false;
     _gameStart = false;
-    _gameTimeNum = 20;
+    _gameTimeNum = 30;
     GameTimeLayer.innerHTML = creatTimeText(_gameTimeNum);
     countBlockSize();
     refreshGameLayer(GameLayer[0]);
